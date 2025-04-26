2507973c09db372ba727a4af560cd4b5ad42b3d1
SophiaMIaoooooooooooo <yuyangmiao113@gmail.com>
2025-04-26 11:17:32 +0800

    Update registerUser.h

--- registerUser.h
+++ registerUser.h
@@ -1,1 +1,13 @@
+#ifndef registerUser_H
+#define registerUser_H
+#include <iostream>
+#include <vector>
 
+struct account {
+    int id;
+    std::string name;
+    int highestScore;
+};
+
+void registerUser(std::vector<account>& users);
+#endif 
