68cc8b29d8e0240446187d7b0ee671b84f19ed40
SophiaMIaoooooooooooo <yuyangmiao113@gmail.com>
2025-04-26 11:45:25 +0800

    Update saveStatus.cpp

--- saveStatus.cpp
+++ saveStatus.cpp
@@ -1,4 +1,20 @@
-#include<iostream>
-using namespace std;
-void save(vector<vector<int>>& board, int score){
-  
+#include <fstream>
+#include <iostream>
+#include <vector>
+#include <string>
+
+void load_game_status(vector<vector<int>>& board, int& score, string& level, const string& filename) {
+    ifstream file(filename);
+    if (file.is_open()) {
+        file >> level;
+        file >> score;
+        for (auto& row : board) {
+            for (int& cell : row) {
+                file >> cell;
+            }
+        }
+        file.close();
+    } else {
+        cerr << "Error: Unable to open file for loading." << endl;
+    }
+}
