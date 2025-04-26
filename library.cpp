12b3cd7b431daf5e11fc0373a52d1cff625374e1
Niii-FY <u3627290@connect.hku.hk>
2025-04-26 16:46:42 +0800

    Update library.cpp

--- library.cpp
+++ library.cpp
@@ -436,10 +436,177 @@ public:
     }
 };
 
+//6*6 Version
+class BigMennyPro {    //BigMenny Pro is a class that stores the current score and level information 
+private:
+    vector<vector<int>> board;
+    int score;
+    string level;
 
+    bool move_single(int& current, int& next) {   // move a single number and change values
+        if (next == 0) {
+            next = current;
+            current = 0;
+            return true;
+        } else if (current == next) {
+            next *= 2;
+            score += next;
+            current = 0;
+            return true;
+        }
+        return false;
+    }
 
+public:
+    BigMennyPro() {
+        reset();
+    }
 
+    void reset() {
+        board = vector<vector<int>>(6, vector<int>(6, 0));  // change the board to 6*6
+        score = 0;
+        level = "easy";
+    }
 
+    void print() {    // print the 6*6 board, current score, level and stauts
+        cout << "\n";
+        for(int i = 0; i < 6; ++i) {  
+            for(int j = 0; j < 6; ++j) {
+                cout << setw(6) << board[i][j];
+            }
+            cout << endl << endl;
+        }
+        cout << "Score: " << score << endl;
+        cout << "Level: " << level << endl;
+        cout << "Status: " << check() << endl;
+    }
+
+    void update(string direction) {   // Update the board according to the given move direction(w/a/s/d)
+        bool moved = false;
+
+        if(direction == "w") {  
+            for(int j = 0; j < 6; ++j) {  
+                for(int i = 1; i < 6; ++i) {
+                    if(board[i][j] == 0) continue;
+                    int k = i;
+                    while(k > 0 && move_single(board[k][j], board[k-1][j])) {
+                        moved = true;
+                        k--;
+                    }
+                }
+            }
+        } else if(direction == "s") {  
+            for(int j = 0; j < 6; ++j) {  
+                for(int i = 4; i >= 0; --i) {
+                    if(board[i][j] == 0) continue;
+                    int k = i;
+                    while(k < 5 && move_single(board[k][j], board[k+1][j])) {
+                        moved = true;
+                        k++;
+                    }
+                }
+            }
+        } else if(direction == "a") {  
+            for(int i = 0; i < 6; ++i) {  
+                for(int j = 1; j < 6; ++j) {
+                    if(board[i][j] == 0) continue;
+                    int k = j;
+                    while(k > 0 && move_single(board[i][k], board[i][k-1])) {
+                        moved = true;
+                        k--;
+                    }
+                }
+            }
+        } else if(direction == "d") {  
+            for(int i = 0; i < 6; ++i) {  
+                for(int j = 4; j >= 0; --j) {
+                    if(board[i][j] == 0) continue;
+                    int k = j;
+                    while(k < 5 && move_single(board[i][k], board[i][k+1])) {
+                        moved = true;
+                        k++;
+                    }
+                }
+            }
+        }
+    }
+
+    void add_random() {  // Add a random new number (2, 4, or 8) in an empty box
+        vector<pair<int, int>> empty_cells;
+        for(int i = 0; i < 6; ++i) {  
+            for(int j = 0; j < 6; ++j) {
+                if(board[i][j] == 0) {
+                    empty_cells.push_back({i, j});
+                }
+            }
+        }
+
+        if(empty_cells.empty()) { return; }
+
+        srand(time(NULL));
+        int index = rand() % empty_cells.size();
+
+        int value;
+        int random_num = rand() % 10;
+
+        if(level == "easy") {
+            if(random_num < 8) value = 2;
+            else if(random_num < 9) value = 4;
+            else value = 8;
+        }
+        else if(level == "medium") {
+            if(random_num < 6) value = 2;
+            else if(random_num < 8) value = 4;
+            else value = 8;
+        }
+        else if(level == "hard") {
+            if(random_num < 4) value = 2;
+            else if(random_num < 7) value = 4;
+            else value = 8;
+        }
+
+        board[empty_cells[index].first][empty_cells[index].second] = value;
+    }
+
+    string check() {  // check the current status: win/playing/lose
+        for(int i = 0; i < 6; ++i) {  
+            for(int j = 0; j < 6; ++j) {
+                if(board[i][j] == 4096) {
+                    return "You've reached 4096!";
+                }
+            }
+        }
+
+        for(int i = 0; i < 6; ++i) {  
+            for(int j = 0; j < 6; ++j) {
+                if(board[i][j] == 0) {
+                    return "playing";
+                }
+            }
+        }
+
+        for(int i = 0; i < 6; ++i) {  
+            for(int j = 0; j < 6; ++j) {
+                if(j < 5 && board[i][j] == board[i][j+1]) {
+                    return "playing";
+                }
+                if(i < 5 && board[i][j] == board[i+1][j]) {
+                    return "playing";
+                }
+            }
+        }
+
+        return "lose";
+    }
+
+    void set_level(string new_level) {
+        level = new_level;
+    }
+
+    int get_score() {
+        return score;
+    }
+};
 
 
 int main() {
