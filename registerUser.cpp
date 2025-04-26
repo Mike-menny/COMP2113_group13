207ad9faed2b2c441208545844af05fe84577557
SophiaMIaoooooooooooo <yuyangmiao113@gmail.com>
2025-04-26 11:14:57 +0800

    Update registerUser.cpp

--- registerUser.cpp
+++ registerUser.cpp
@@ -1,3 +1,17 @@
-#include<iostream>
+#include <iostream>
+#include <vector>
 using namespace std;
 
+struct account {
+  int id;
+  string name;
+  int highestScore;
+};
+
+void registerUser(vector<account>& users) {
+  cout << "Enter your name:\n";
+  string name;
+  getline(cin, name);
+  account Acc = {users.size(), name, 0};
+  users.push_back(Acc);
+}
