6f278a7fd72d625ab2cab68947af518fa949e8e8
SophiaMIaoooooooooooo <yuyangmiao113@gmail.com>
2025-04-26 11:49:46 +0800

    Create save.cpp

---
+++ save.cpp
@@ -0,0 +1,21 @@
+#include<iostream>
+#include<fstream>
+#include<vestor>
+#include<string>
+using namespace std;
+void save_game_status(vector<vector<int>>& board, int score, string& level, string& filename){
+  ofstream file(filename);
+  if(file.is_open()){
+    file << level << std::endl;
+        file << score << std::endl;
+        for (const auto& row : board) {
+            for (int cell : row) {
+                file << cell << " ";
+            }
+            file << std::endl;
+        }
+        file.close();
+  } else {
+    cerr<<"Unable to open file!\n";
+  }
+}
