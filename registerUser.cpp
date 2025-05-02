3c71740d3e42ea7a3ab1843fb9778efa1d35e026
SophiaMIaoooooooooooo <yuyangmiao113@gmail.com>
2025-05-02 20:54:09 +0800

    Update registerUser.cpp

--- registerUser.cpp
+++ registerUser.cpp
@@ -5,17 +5,32 @@
 namespace fs = std::experimental::filesystem;
 using namespace std;
 
+/**
+ * @struct account
+ * @brief Stores player account information including name and highest scores for different levels
+ * 
+ * @var account::name 
+ * The player's username
+ * @var account::highestScore4
+ * Highest score achieved on level 4
+ * @var account::highestScore5 
+ * Highest score achieved on level 5
+ * @var account::highestScore6
+ * Highest score achieved on level 6
+ */
 struct account {
   string name;
   int highestScore4;
   int highestScore5;
   int highestScore6;
 };
-/**
- * @param users the user record vector
- * @return none, since user record have been passed by reference
- */
 
+/**
+ * @brief Checks if a username already exists by looking for corresponding .txt files
+ * 
+ * @param name The username to check for availability
+ * @return true if username is already taken (file exists), false otherwise
+ */
 bool checkuser(string name){
   for (const auto& entry : fs::directory_iterator(".")) {
     if ((name+".txt")==(entry.path().filename())){
@@ -25,6 +40,13 @@ bool checkuser(string name){
   return false;
 }
 
+/**
+ * @brief Registers a new user by creating an account structure and saving file
+ * 
+ * @return account The newly created user account
+ * @note Prompts for username and verifies it's unique before creating
+ * @warning Will continue prompting until unique username is provided
+ */
 account registerUser() {
   account user;
   user.highestScore4 = 0;
@@ -46,6 +68,14 @@ account registerUser() {
   return user;
 }
 
+/**
+ * @brief Loads user data from file into an account structure
+ * 
+ * @param filename The name of the user's data file (username.txt)
+ * @param user Reference to account struct where data will be loaded
+ * @note File format must be: name\nscore4\nscore5\nscore6
+ * @warning Does not validate file contents, assumes proper formatting
+ */
 void readuser(string filename, account &user){
   ifstream fin;
   fin.open(filename);
@@ -58,6 +88,13 @@ void readuser(string filename, account &user){
   }
 }
 
+/**
+ * @brief Saves user account data to their personal file
+ * 
+ * @param user Reference to account struct containing data to save
+ * @note Creates or overwrites file in format: name\nscore4\nscore5\nscore6
+ * @warning Will overwrite existing file without confirmation
+ */
 void saveUser(account &user){
   ofstream fout;
   fout.open(user.name+".txt");
@@ -71,4 +108,3 @@ void saveUser(account &user){
     cerr << "Error: Could not save user data to file." << endl;
   }
 }
-
